931271173e499b613bebf50583aaa7117e975e7b
creativexi <av133x@gmail.com>
2017-05-20 20:48:49 +0300

    home

--- src/less8/Recursion.java
+++ src/less8/Recursion.java
@@ -1,4 +1,16 @@
 package less8;
 
 public class Recursion {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                System.out.println(i);
+            }
+        } else {
+            for (int i = a; i >= b; i--) {
+                System.out.println(i);
+            }
+        }
+    }
 }
