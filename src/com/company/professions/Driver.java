c65e4eda588b993c6de266e063d4d6bbdfe107ef
creativexi <av133x@gmail.com>
2017-05-23 14:00:07 +0300

    home

--- src/com/company/professions/Driver.java
+++ src/com/company/professions/Driver.java
@@ -3,8 +3,20 @@ package com.company.professions;
 public class Driver {
     private String fullName;
     private int exp;
-    public int getExp(){return exp;}
-    public String getFullName(){return fullName;}
-    public void setFullName(String fullName){this.fullName = fullName;}
-    public void setExp(int exp){this.exp = exp;}
+
+    public int getExp() {
+        return exp;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
 }
--- src/com/company/vechicles/Car.java
+++ src/com/company/vechicles/Car.java
@@ -8,24 +8,46 @@ public class Car {
     private String clas;
     private int veight;
 
-    private int getVeight() {return veight;}
-    private String getMark() {return mark;}
-    private String getClas() {return clas;}
-    private void setVeight(int veight) {this.veight = veight;}
-    private void setMark(String mark) {this.mark = mark;}
-    private void setClas(String clas) {this.clas = clas;}
+    private int getVeight() {
+        return veight;
+    }
+
+    private String getMark() {
+        return mark;
+    }
+
+    private String getClas() {
+        return clas;
+    }
+
+    private void setVeight(int veight) {
+        this.veight = veight;
+    }
+
+    private void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    private void setClas(String clas) {
+        this.clas = clas;
+    }
+
     private void start() {
         System.out.println("Поехали");
     }
+
     private void stop() {
         System.out.println("Останавливаемся");
     }
+
     private void left() {
         System.out.println("Поворот налево");
     }
+
     private void right() {
         System.out.println("Поворот направо");
     }
+
     private void printInfo(Car car, Engine engine, Driver driver) {
         System.out.println("Машина: " + car.getMark() + " класса: " + car.getClas() + ". Вес: " + car.getVeight() + "кг");
         System.out.println("Водитель: " + driver.getFullName() + ", стаж вождения: " + driver.getExp());
@@ -49,6 +71,4 @@ public class Car {
         car.start();
         car.stop();
     }
-
-
 }
