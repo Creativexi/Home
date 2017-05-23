a544080be02116536eadcca2d53d2d194c289dd5
creativexi <av133x@gmail.com>
2017-05-23 12:41:52 +0300

    home

--- src/less8/flower/Flower.java
+++ src/less8/flower/Flower.java
@@ -1,38 +1,59 @@
 package less8.flower;
 
-/**
- * Создать класс "Flower", который содержит переменные имя и стоимость.
- * Собрать 3 букета (используем массив) с определением их стоимости.
- * Также подсчитать количество проданных цветов (используем статический метод).
- */
-public class Flower extends Booket {
-    public String name;
-    public int cost;
+public class Flower {
+    private String name;
+    private int cost;
     private static int flowerCount;
 
-    public static int getFlowerCount() {
-        return flowerCount;
-    }
-
-    public String getName() {
+    private String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCost() {
+    private int getCost() {
         return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
     }
 
     public Flower(String name, int cost) {
         this.name = name;
         this.cost = cost;
         flowerCount++;
+    }
+
+    public static void printBooketInfo(Flower[] booket) {
+        int flowerCountRose = 0;
+        int flowerCountLily = 0;
+        int flowerCountDaisy = 0;
+        int flowerCountLotus = 0;
+        int flowerCountAster = 0;
+        int price = 0;
+        for (Flower flower : booket) {
+            price += flower.getCost();
+            if (flower.getName().equals("Rose")) {
+                flowerCountRose = ++flowerCountRose;
+            }
+            if (flower.getName().equals("Lily")) {
+                flowerCountLily = ++flowerCountLily;
+            }
+            if (flower.getName().equals("Aster")) {
+                flowerCountAster = ++flowerCountAster;
+            }
+            if (flower.getName().equals("Lotus")) {
+                flowerCountLotus = ++flowerCountLotus;
+            }
+            if (flower.getName().equals("Daisy")) {
+                flowerCountDaisy = ++flowerCountDaisy;
+            }
+        }
+        System.out.println("Букет состоит из:");
+        System.out.println("Роз: " + flowerCountRose + " шт");
+        System.out.println("Лилий: " + flowerCountLily + " шт");
+        System.out.println("Астр: " + flowerCountAster + " шт");
+        System.out.println("Лотосов: " + flowerCountLotus + " шт");
+        System.out.println("Маргариток: " + flowerCountDaisy + " шт");
+        System.out.println("Стоимость букета: " + price);
+    }
+
+    public static void flowerSold() {
+        System.out.println("Всего цветков было продано: " + flowerCount + " шт");
     }
 }
--- src/less8/flower/FlowerDemo.java
+++ src/less8/flower/FlowerDemo.java
@@ -1,33 +1,33 @@
 package less8.flower;
-
+import static less8.flower.Flower.flowerSold;
+import static less8.flower.Flower.printBooketInfo;
 
 public class FlowerDemo {
     public static void main(String[] args) {
-
-        Booket booket1[] = new Booket[]{
-            new Flower("Rose", 20),
-            new Flower("Lotus", 25),
-            new Flower("Lily", 15),
-            new Flower("Daisy", 10),
-            new Flower("Aster", 22),
-        };
-        Booket booket2[] = new Booket[]{
+        Flower booket1[] = new Flower[]{
                 new Flower("Rose", 20),
                 new Flower("Lotus", 25),
                 new Flower("Lily", 15),
                 new Flower("Daisy", 10),
                 new Flower("Aster", 22),
         };
-        Booket booket3[] = new Booket[]{
+        Flower booket2[] = new Flower[]{
                 new Flower("Rose", 20),
-                new Flower("Lotus", 25),
+                new Flower("Rose", 20),
+                new Flower("Rose", 20),
+                new Flower("Rose", 20),
+                new Flower("Rose", 20),
+        };
+        Flower booket3[] = new Flower[]{
                 new Flower("Lily", 15),
-                new Flower("Daisy", 10),
+                new Flower("Lily", 15),
+                new Flower("Lily", 15),
+                new Flower("Aster", 22),
                 new Flower("Aster", 22),
         };
+        printBooketInfo(booket1);
+        flowerSold();
     }
+}
 
-
-
-        }
 
