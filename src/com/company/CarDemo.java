1a54b6967e89456a9f4ed45c25fcaca8d8a3743f
creativexi <av133x@gmail.com>
2017-05-23 15:08:59 +0300

    home

---
+++ src/com/company/CarDemo.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import com.company.details.Engine;
+import com.company.professions.Driver;
+import com.company.vechicles.Car;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Engine engine = new Engine();
+        Driver driver = new Driver();
+        engine.setManufacturer("BMW");
+        engine.setPower(200);
+        car.setClas("S");
+        car.setMark("KIA");
+        car.setVeight(1000);
+        driver.setFullName("Владимир");
+        driver.setExp(5);
+        car.printInfo(car, engine, driver);
+        car.left();
+        car.right();
+        car.start();
+        car.stop();
+    }
+}
--- src/com/company/vechicles/Car.java
+++ src/com/company/vechicles/Car.java
@@ -20,55 +20,37 @@ public class Car {
         return clas;
     }
 
-    private void setVeight(int veight) {
+    public void setVeight(int veight) {
         this.veight = veight;
     }
 
-    private void setMark(String mark) {
+    public void setMark(String mark) {
         this.mark = mark;
     }
 
-    private void setClas(String clas) {
+    public void setClas(String clas) {
         this.clas = clas;
     }
 
-    private void start() {
+    public void start() {
         System.out.println("Поехали");
     }
 
-    private void stop() {
+    public void stop() {
         System.out.println("Останавливаемся");
     }
 
-    private void left() {
+    public void left() {
         System.out.println("Поворот налево");
     }
 
-    private void right() {
+    public void right() {
         System.out.println("Поворот направо");
     }
 
-    private void printInfo(Car car, Engine engine, Driver driver) {
+    public void printInfo(Car car, Engine engine, Driver driver) {
         System.out.println("Машина: " + car.getMark() + " класса: " + car.getClas() + ". Вес: " + car.getVeight() + "кг");
         System.out.println("Водитель: " + driver.getFullName() + ", стаж вождения: " + driver.getExp());
         System.out.println("Производитель двигателя: " + engine.getManufacturer() + ", мощность: " + engine.getPower());
-    }
-
-    public static void main(String[] args) {
-        Car car = new Car();
-        Engine engine = new Engine();
-        Driver driver = new Driver();
-        engine.setManufacturer("BMW");
-        engine.setPower(200);
-        car.setClas("S");
-        car.setMark("KIA");
-        car.setVeight(1000);
-        driver.setFullName("Владимир");
-        driver.setExp(5);
-        car.printInfo(car, engine, driver);
-        car.left();
-        car.right();
-        car.start();
-        car.stop();
     }
 }
