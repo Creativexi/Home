60cd2e6f3ee643bebb9e2ef4eab207ac6637365f
creativexi <av133x@gmail.com>
2017-07-02 22:36:50 +0300

    home

--- src/less12/EngWordsNumber.java
+++ src/less12/EngWordsNumber.java
@@ -1,8 +1,30 @@
 package less12;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  Найти количество слов, содержащих только символы латинского алфавита.
  */
 public class EngWordsNumber {
+    public static void main(String[] args) {
 
+        String l = "Regular expressions opens перед вами возможности, о которых вы, возможно, даже не подозревали.";
+        Matcher m = Pattern.compile("[A-Za-z]+").matcher(l);
+        ArrayList<String> words = new ArrayList<>();
+        while (m.find()) {
+            words.add(l.substring(m.start(), m.end()));
+        }
+
+        System.out.println(getEngNum(words));
+    }
+
+    public static int getEngNum(ArrayList<String> words) {
+        int num = 0;
+        for (String word : words) {
+           num++;
+            }
+        return num;
+    }
 }
